94ed66820a14ae45088e5611eae66cac9eacf6c0
m-iftikhar <iftikharrj2001@gmail.com>
2025-01-13 15:59:37 +0500

    home insert components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
 import Featurecollection from "../components/Featurecollection";
 import Hero from "../components/Hero";
+import TopSeller from "../components/TopSeller";
 import Trendings from "../components/Trendings";
+import Auction from '../components/Auction';
+import TopCollections from "../components/TopCollection";
+import Recentlysold from "../components/Recentlysold";
+import DigitalAsset from '../components/DigitalAsset'
+import Boost from "../components/Boost"
+import Questions from '../components/Questions'
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className="bg-gradient-to-b from-white to-pink-100 min-h-screen">
+    <div className="bg-gradient-to-b from-white to-pink-200 min-h-screen">
       <Hero />
       <Featurecollection/>
-      <Trendings/>
+      <Trendings title="Trending NFTs"/>
+      <TopSeller/>
+      <Auction/>
+      <TopCollections/>
+      <Recentlysold/>
+      <DigitalAsset/>
+      <Boost/>
+      <Questions/>
+      <Footer/>
     </div>
   );
 };
