64366a7344e7d8aad134bd813260679cb5275d1f
m-iftikhar <iftikharrj2001@gmail.com>
2025-01-13 15:57:28 +0500

    routes set

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Nftpage from "./pages/Nftpage";
+
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Home/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nftpage" element={<Nftpage />} />
+      </Routes>
+    </Router>
   );
 };
 
