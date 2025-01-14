938e9911be27ad3fcb2e71a65ac75dbf0b1ad995
m-iftikhar <iftikharrj2001@gmail.com>
2025-01-14 15:59:11 +0500

    routes set

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Nftpage from "./pages/Nftpage";
+import Nftmintpage from './pages/Nftmintpage'
+import Mintdata from "./pages/Mintdata";
+import Topcollect from "./pages/Topcollect";
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/nftpage" element={<Nftpage />} />
+        <Route path="/nftpagemint" element={<Nftmintpage />} />
+        <Route path="/mintdata" element={<Mintdata />} />
+        <Route path="/topcollections" element={<Topcollect />} />
       </Routes>
     </Router>
   );
