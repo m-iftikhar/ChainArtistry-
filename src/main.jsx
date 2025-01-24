4f6426160bd4fba2d602fe0e9b3169549078458a
m-iftikhar <iftikharrj2001@gmail.com>
2025-01-24 09:50:12 +0500

    globally undefined

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (typeof global === 'undefined') {
+  var global = window;
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
