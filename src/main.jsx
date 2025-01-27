632bcbd69b9e99bdf90caafbc7f103b13ab15b8b
m-iftikhar <iftikharrj2001@gmail.com>
2025-01-27 10:02:18 +0500

    pakage unistall

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Only assign to global if it's undefined in a non-browser environment.
 if (typeof global === 'undefined') {
   var global = window;
 }
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
